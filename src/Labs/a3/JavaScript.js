cfa0dbe8962ab44e58079e3272669839d8348f21
abhishekkumar <kumar.abhishe@northeastern.edu>
2023-10-17 10:25:47 -0400

    run

--- src/Labs/a3/JavaScript.js
+++ src/Labs/a3/JavaScript.js
@@ -1,8 +1,18 @@
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+
 function JavaScript() {
+
     console.log("Hello World!");
     return(
        <div>
           <h1>JavaScript</h1>
+          <VariablesAndConstants/>
+          <VariableTypes/>
+          <IfElse/>
+          <TernaryOperator/>
        </div>
     );
  }
