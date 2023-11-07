8a0f573409ae69f13e8065fd4cd46e028f7f89f0
abhishekkumar <kumar.abhishe@northeastern.edu>
2023-11-07 09:45:42 -0500

    Done until before AJAX 3.4.1

--- src/Nav.js
+++ src/Nav.js
@@ -4,9 +4,12 @@ function Nav() {
   return (
     <nav className="nav nav-tabs mt-2">
       <Link to="/Labs/a3"
-            className={`nav-link ${pathname.includes("a3") ? "active" : ""}`}>A3</Link>
+            className={`nav-link ${pathname.includes("a3") ? "active" : ""}`}>Assignmet 3</Link>
       <Link to="/Labs/a4"
-            className={`nav-link ${pathname.includes("a4") ? "active" : ""}`}>A4</Link>
+            className={`nav-link ${pathname.includes("a4") ? "active" : ""}`}>Assignment 4</Link>
+      <Link to="/Labs/a5"
+            className={`nav-link ${pathname.includes("a5") ? "active" : ""}`}>Assignment 5</Link>
+
       <Link to="/hello"
             className={`nav-link ${pathname.includes("hello") ? "active" : ""}`}>Hello</Link>
       <Link to="/Kanbas"
